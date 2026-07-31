76102c7be004186f9b80ab30ace93ffdfccebe45
Nishal-15 <nishalnishith@gmail.com>
2026-08-01 01:23:50 +0530

    Remove temporary backend utility scripts

--- backend/fix_priya.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-    const db = mongoose.connection.db;
-    await db.collection('lawyers').updateMany({}, { $set: { isBlocked: false } });
-    console.log('Unblocked all lawyers in the DB to sync with Users DB');
-    process.exit(0);
-});
